4d97fd8049708f0bbb587d04b38f4de8a0eee1ef
Luana Acosta <luana.acosta.005@gmail.com>
2023-10-10 19:28:58 -0300

    intento de algo

--- main.js
+++ main.js
@@ -1,66 +1,99 @@
-let IngreseNombre = prompt("Bienvenido ingrese su Nombre");
-alert("Bienvenido " + IngreseNombre);
+// // declaracion
+// let Productos; ${Productos}
+// let Contactarnos; ${contactarnos}
+// let Salir; ${salir}
 
-let opciones = prompt(`Que se te ofrece? 
-    1. Ver nuestros productos.
-    2. Contactarnos.
-    3. Salir 
-`);
 
-if (respuesta === "1") {
-  alert("Podras ver nuestros productos");
-} else if (respuesta ==="2") {
-  alert("Comunicate al -54 11-0230-9045");
-} else (respuesta =="3") {
-    alert("GRACIAS POR TU VISITA, ESPERAMOS VERTE PRONTO!!! ;)");
+let IngreseNombre = prompt("Hola, Cual es tu nombre?");
+  alert("Bienvenido/a " + IngreseNombre);
+  
+let op = prompt(`
+  Que se te ofrece? 
+    1. Ver nuestros productos. 
+    2. Contactarnos. 
+    3. Salir. 
+    Elije una opcion (num)...
+    `);
+ 
+while (op)
+console.log ("op")
+    switch (op) {
+      case "1":
+        alert(`Podras ver nuestros productos`);
+        break;
+  
+      case "2":
+        alert(`Comunicate al -54 11-0230-9045`);
+        break;
+  
+        case "3":
+        alert(`GRACIAS POR TU VISITA, ESPERAMOS VERTE PRONTO!!!`);
+        break;
+
+      default:
+        alert(`Elija una opcion (num)...`);
+        break;
 }
+         
+    
+  //    let Productos = prompt(" Selecciona el
+  //    Accesorios, higiene, cosmeticos, indumentaria
+  //  ") 
 
-let productos = "Productos disponibles";
+// let productos = "Productos disponibles";
 
-switch (productos) {
-  case "Accesorios":
-    console.log("Aritos, Collares y pulseras de todo tipo");
-    break;
+// switch (Productos) {
+//   case "Accesorios":
+//     console.log("Aritos, Collares y pulseras de todo tipo");
+//     break;
 
-  case "Higiene":
-    console.log("Jabones, cremas de baño, Shampoos, acondicionadores, toallitas, desodorantes");
-    break;
+//   case "Higiene":
+//     console.log("Jabones, cremas de baño, Shampoos, acondicionadores, toallitas, desodorantes");
+//     break;
 
-  case "Cosmeticos":
-    console.log("Crema para el cuerpo, limpiadores faciales,  protectores solares ");
-    break;
+//   case "Cosmeticos":
+//     console.log("Crema para el cuerpo, limpiadores faciales,  protectores solares ");
+//     break;
 
-  default:
-    console.log("Moneda no reconocida");
-    break;
-}
+//   default:
+//     console.log("");
+//     break;
+// }
 
-let opcion = prompt(`
-  Ingrese la opción requerida
+// let opcion = prompt(`
+//   Ingrese la opción requerida
 
-  1. Ropa Mujer
-  2. Ropa Hombre
-`);
+//   1. Ropa Mujer
+//   2. Ropa Hombre
+// `);
 
-while (opcion != "salir") {
-  switch (opcion) {
-    case "1":
-      alert(`Tenemos camisas y pantalones`);
-      break;
+// while (opcion != "salir") {
+//   switch (opcion) {
+//     case "1":
+//       alert(`Tenemos camisas y pantalones`);
+//       break;
 
-    case "2":
-      alert(`Tenemos zapatos y pantalones`);
-      break;
+//     case "2":
+//       alert(`Tenemos zapatos y pantalones`);
+//       break;
 
-    default:
-      alert("Opción incorrecta");
-      break;
-  }
+//     default:
+//       alert("Opción incorrecta");
+//       break;
+//   }
 
-  opcion = prompt(`
-    Ingrese la opción requerida
+//   let opcion = prompt(`
+//     Ingrese la opción requerida
 
-    1. Ropa Mujer
-    2. Ropa Hombre
-  `);
-}
+//     1. Ropa Mujer
+//     2. Ropa Hombre
+//   `);
+// }
+// if (Productos === "1"); {
+//   alert("Podras ver nuestros productos");
+//   Productos = Accesorios, higiene, cosmeticos, indumentaria
+// } else if (contactarnos == "2"); {
+//   alert("Comunicate al -54 11-0230-9045");
+// } else (salir || "3"); {
+//   alert("GRACIAS POR TU VISITA, ESPERAMOS VERTE PRONTO!!! ;)");
+// }
